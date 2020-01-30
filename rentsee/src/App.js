d0f307941fc68a3b1b2dbbdabdb845bca07ff715
namkang321 <namkang_chutisilp@hotmail.com>
2020-01-30 17:57:34 +0700

    add Login, Register, Home page

---
+++ rentsee/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Register from './Register/Register';
+import Home from './Home/Home';
+import Login from './Login/Login';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <ul>
+                    <a href='/'>Home</a>
+                    <a href='/login'>Login</a>
+                    <a href='/register'>Register</a>
+                </ul>
+                <Router>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/register' component={Register} />
+                </Router>
+            </header>
+        </div>
+    );
+}
+
+export default App;
