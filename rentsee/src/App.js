c4dece35d1ff28fe6b5fee30459d670f648d72fa
namkang321 <namkang_chutisilp@hotmail.com>
2020-02-05 13:55:29 +0700

    decorate Login page

--- rentsee/src/App.js
+++ rentsee/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Register from './Register/Register';
 import Home from './Home/Home';
 import Login from './Login/Login';
--- rentsee/src/Login/Login.jsx
+++ rentsee/src/Login/Login.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import './Login.css';
 
 class Login extends Component {
     state = {};
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <form method='post'>
-                    <div>
-                        Username:
-                        <input type='text' name='username' />
+            <div className='container-fluid full-screen'>
+                <div className='row'>
+                    <div className='col-lg-6 col-xs-0'>
+                        <div className='row login-banner'>
+                            <div className='login-inner-banner'></div>
+                        </div>
                     </div>
-                    <div>
-                        Password:
-                        <input type='password' name='password' />
+                    <div className='col-lg-6 col-xs-4'>
+                        <h1>Login</h1>
+                        <form method='post'>
+                            <div>
+                                Username:
+                                <input type='text' name='username' />
+                            </div>
+                            <div>
+                                Password:
+                                <input type='password' name='password' />
+                            </div>
+                            <input type='submit' value='Submit' />
+                        </form>
                     </div>
-                    <input type='submit' value='Submit' />
-                </form>
+                </div>
             </div>
         );
     }
