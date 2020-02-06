d472ebc180727e8580a5a5ef053d7c47c284ba83
namkang321 <namkang_chutisilp@hotmail.com>
2020-02-06 12:16:47 +0700

    Register: add basic register function (not decorated yet)

--- rentsee/src/Register/Register.jsx
+++ rentsee/src/Register/Register.jsx
@@ -1,41 +1,158 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
-    state = {};
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullname: '',
+            birthdate: '',
+
+            username: '',
+            password: '',
+
+            drivingLicense: '',
+            email: '',
+            bankAccountNumber: '',
+            phoneNumber: '',
+            creditCardNumber: ''
+        };
+
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleFormChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        console.log('change: ' + [name] + ' ' + value);
+        this.setState({ [name]: value });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        fetch('http://rentsee.krist7599555.ml/api/register', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    alert('Request is fucked up');
+                }
+            })
+            .then(resJson => {
+                if (resJson) {
+                    localStorage.setItem('userInfo', JSON.stringify(resJson));
+                    this.props.history.push('/');
+                }
+            })
+            .catch(error => {
+                console.log('error: ', error);
+            });
+    }
     render() {
         return (
             <div>
                 <h1>Register</h1>
-                <form method='post'>
-                    <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className='form-group'>
+                        <label>
+                            Fullname
+                            <input
+                                className='form-control'
+                                name='fullname'
+                                onChange={this.handleFormChange}
+                                placeholder='Fullname'
+                            />
+                        </label>
+                        <label>
+                            Date of Birth
+                            <input
+                                className='form-control'
+                                name='birthdate'
+                                onChange={this.handleFormChange}
+                                type='date'
+                                placeholder='Date of Birth'
+                            />
+                        </label>
+                    </div>
+                    <label>
                         Username:
-                        <input placeholder='username' />
-                    </div>
-                    <div>
+                        <input
+                            className='form-control'
+                            onChange={this.handleFormChange}
+                            name='username'
+                            placeholder='username'
+                        />
+                    </label>
+                    <label>
                         Password:
-                        <input type='password' placeholder='password' />
-                    </div>
-                    <div>
+                        <input
+                            className='form-control'
+                            onChange={this.handleFormChange}
+                            name='password'
+                            type='password'
+                            placeholder='password'
+                        />
+                    </label>
+                    <label>
                         Driving License:
-                        <input type='file' name='pic' accept='image/*' />
-                    </div>
-                    <div>
+                        <input
+                            className='form-control-file'
+                            onChange={this.handleFormChange}
+                            name='drivingLicense'
+                            type='file'
+                            accept='image/*'
+                        />
+                    </label>
+                    <label>
                         E-Mail:
-                        <input type='text' placeholder='e-mail address' />
-                    </div>
-                    <div>
+                        <input
+                            className='form-control'
+                            onChange={this.handleFormChange}
+                            name='email'
+                            type='text'
+                            placeholder='e-mail address'
+                        />
+                    </label>
+                    <label>
                         Bank Account Number:
-                        <input type='number' placeholder='bank account number' />
-                    </div>
-                    <div>
+                        <input
+                            className='form-control'
+                            name='bankAccoutNumber'
+                            onChange={this.handleFormChange}
+                            type='number'
+                            placeholder='bank account number'
+                        />
+                    </label>
+                    <label>
                         Phone Number:
-                        <input type='number' placeholder='phone number' />
-                    </div>
-                    <div>
+                        <input
+                            className='form-control'
+                            name='phoneNumber'
+                            onChange={this.handleFormChange}
+                            type='number'
+                            placeholder='phone number'
+                        />
+                    </label>
+                    <label>
                         Credit Card Number:
-                        <input type='number' placeholder='credit card number' />
-                    </div>
-                    <input type='submit' value='Submit' />
+                        <input
+                            className='form-control'
+                            name='creditCardNumber'
+                            onChange={this.handleFormChange}
+                            type='number'
+                            placeholder='credit card number'
+                        />
+                    </label>
+                    <input className='btn mt-4' type='submit' value='Submit' />
                 </form>
             </div>
         );
